fc3f2995a05a41b6f2e5e55dfe06adbbace032d2
AdrianPalmadev <adrianmilwid@gmail.com>
2025-02-13 11:06:30 +0100

    modified:   Exercise03.php

--- Exercise03.php
+++ Exercise03.php
@@ -1,3 +1,22 @@
+<?php
+
+session_start();
+
+foreach (['product', 'quantity', 'price'] as $key) {
+    if (!isset($_SESSION[$key])) {
+        $_SESSION[$key] = [];
+    }
+}
+
+if (isset($_POST['add'])) {
+    $name = $_POST['name'];
+    $quantity = $_POST['quantity'];
+    $price = $_POST['price'];
+    $_SESSION['product'] = $name;
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +27,21 @@
 </head>
 
 <body>
+    <h1>Shopping list</h1>
+    <form method="POST" action="#">
+        <label for="name">name:</label>
+        <input type="text" id="name" name="name"><br><br>
 
+        <label for="quantity">quantity:</label>
+        <input type="number" id="quantity" name="quantity"><br><br>
+
+        <label for="price">price:</label>
+        <input type="number" id="price" name="price"><br><br>
+
+        <input type="submit" name="add" value="Add">
+        <input type="submit" name="upd" value="Update">
+        <input type="reset" value="Reset">
+    </form>
 </body>
 
 </html>
